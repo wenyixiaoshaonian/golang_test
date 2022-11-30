ad473ee0f88f7361ae3e03306e8303601473e939
caoboxi <541190157@qq.com>
2022-11-30 17:24:01 +0800

    channel finished

--- gorou.go
+++ gorou.go
@@ -25,28 +25,40 @@ func gofunc(s string) {
 
 func main() {
 	runtime.GOMAXPROCS(1)
-	wg.Add(2)
-	go gofunc("go coroutine function")
+	wg.Add(1)
+	chanName := make(chan string,5)
+	// go gofunc("go coroutine function")
 
-	go func (s string) {
-		t := 0
+	go func () {
 		defer wg.Done()
 		for i:= 0 ; i < 3 ; i++ {
-			fmt.Println("Info =", s,"2 times : ",t)
-			// 延时1秒
-			time.Sleep(time.Second)
-			t++
-			defer countMux.RUnlock()
-			countMux.RLock()
-			fmt.Println(a)
+			chanName <- "go coroutine function"
+			fmt.Println("send once.....")
 		}
-	}("go coroutine function")
-	wg.Wait()
-	fmt.Println("11111")
-	// for {
-	// 	fmt.Println("Info =", "main function","times : ",t)
-	// 	// 延时1秒
-	// 	time.Sleep(time.Second)
-	// 	t++
-	// }
+		// close(chanName)
+		time.Sleep(time.Second)
+	}()
+	// wg.Wait()
+	for {
+		time.Sleep(time.Second)
+		time.Sleep(time.Second)
+		select {
+		case msg,ok := <- chanName :
+			if ok {
+				fmt.Println("chan Msg =", msg," ok = ",ok)
+			} else {
+				fmt.Println("chan is closed"," ok = ",ok)
+				break
+			}
+		case chanName <- "main send" :
+			fmt.Println("main send")
+		case <-time.After(3 * time.Second):
+			fmt.Println("TimeOut")
+			wg.Wait()
+			fmt.Println("finished")
+			return
+		default :
+			fmt.Println("default")
+		}
+	}	
 }
